8171a7fc59cf37990950fa7a216f43c602496fa3
alex-krest <73220512+alex-krest@users.noreply.github.com>
2022-02-10 11:43:03 +0200

    Второе

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,20 +1,21 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 
+let finishDate = 0;
+
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    if (selectedDates[0] < options.defaultDate) {
-      return alert('Please choose a date in the future');
-    }
-    const time = selectedDates[0].getTime() - options.defaultDate.getTime();
-    console.log(time);
-    return time;
+    //  time = selectedDates[0].getTime() - options.defaultDate.getTime();
+    finishDate = selectedDates[0].getTime();
+    console.log(finishDate);
   },
 };
+// НЕ МЕНЯЕТСЯ!!!!!
+console.log(finishDate);
 // ---------------------------------------------
 const refs = {
   inputEl: document.querySelector('input#datetime-picker'),
@@ -24,45 +25,51 @@ const refs = {
   minutesEl: document.querySelector('.value[data-minutes]'),
   secondsEl: document.querySelector('.value[data-seconds]'),
 };
-console.log(refs.dayEl.textContent);
 // ----------------------------------------------
 flatpickr(refs.inputEl, options);
-// --------------------------------------------------
-refs.startEl.addEventListener('click', onStart);
-
-function onStart() {
-  setInterval(() => {
-    const { days, hours, minutes, seconds } = convertMs(options.onClose);
-    refs.dayEl.textContent = days;
-    refs.hoursEl.textContent = hours;
-    refs.minutesEl.textContent = minutes;
-    refs.secondsEl.textContent = seconds;
-  }, 1000);
-  refs.startEl.disabled = true;
+// -По кнопке включаем таймер
+refs.startEl.addEventListener('click', timer);
+// -----------------------------------------------------
+// -Берем обьект из функции convertMs и присваиваем счетчику в интерфейсе данные
+function updateClock({ days, hours, minutes, seconds }) {
+  refs.dayEl.textContent = days;
+  refs.hoursEl.textContent = hours;
+  refs.minutesEl.textContent = minutes;
+  refs.secondsEl.textContent = seconds;
 }
-// -------------------------------------------------
+// -Добавляет 0 к одиночному числу
 function addLeadingZero(value) {
   return String(value).padStart(2, '0');
 }
+// --Высчитывает к-во минут, часов, дней из к-ва секунд+обернута вышепрописанной функцией
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
-
-  // Remaining days
   const days = addLeadingZero(Math.floor(ms / day));
-  // Remaining hours
   const hours = addLeadingZero(Math.floor((ms % day) / hour));
-  // Remaining minutes
   const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
-  // Remaining seconds
   const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
-
   return { days, hours, minutes, seconds };
 }
-
-// console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-// console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-// console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+// --Коллбек функция для кнопки старта
+function timer() {
+  let interval = null;
+  refs.startEl.setAttribute('disabled', 'disabled');
+  // --Дествие с частотой 1 раз в секунду
+  interval = setInterval(counterTime, 1000);
+  //  Колбек функция для вышенаписанной функции.
+  function counterTime() {
+    const currentDate = Date.now();
+    const differenceTime = finishDate - currentDate;
+    if (differenceTime < 0) {
+      clearInterval(interval);
+      return alert('Please choose a date in the future');
+    }
+    //  В переменной вызываем функцию и передаем ей разницу между временем сейчас и выбранной датой в секундах
+    const time = convertMs(differenceTime);
+    //  Вызываем функйию и передаем ей обьект с днями, часами, минутами и секундами для присваивания их счетчику в интерфейсе
+    updateClock(time);
+  }
+}
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,26 @@
+const refs = {
+  formEl: document.querySelector('.form'),
+  delayEl: document.querySelector('[name=delay]'),
+  stepEl: document.querySelector('[name=step]'),
+  amountEl: document.querySelector('[name=amount]'),
+  buttonEl: document.querySelector('form>button'),
+};
+console.log(refs.formEl);
+console.log(refs.delayEl);
+console.log(refs.stepEl);
+console.log(refs.amountEl);
+console.log(refs.buttonEl);
+// --------------------------------------------------
+refs.buttonEl.addEventListener('submit', createPromise);
+
+//
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  consttimerId = setInterval(() => {
+    const shouldResolve = Math.random() > 0.3;
+    if (shouldResolve) {
+      // Fulfill
+    } else {
+      // Reject
+    }
+  }, delay);
 }
