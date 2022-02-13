79245bfa3cb961f36d6e9ac3d7d9a72b1be1be07
alex-krest <73220512+alex-krest@users.noreply.github.com>
2022-02-13 13:58:05 +0200

    Ещё раз

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -7,69 +7,54 @@ const refs = {
   buttonEl: document.querySelector('form>button'),
 };
 // -Взяли значения полей формы:
-const delay = Number(refs.formEl.elements.delay.value);
-const step = Number(refs.formEl.elements.step.value);
-const amount = Number(refs.formEl.elements.amount.value);
-console.log(delay);
-console.log(step);
-console.log(amount);
-// ----Функция отправки формы
-refs.formEl.addEventListener('submit', createPromise);
-// -------------------------------
-function createPromise(position, delay) {
-  const data = {
-    position: position.value,
-    delay: delay.value,
-  };
-  // --ПРОМИС-------------------------
+// const delay = Number(refs.formEl.elements.delay.value);
+// const step = Number(refs.formEl.elements.step.value);
+// const amount = Number(refs.formEl.elements.amount.value);
+// console.log(delay);
+// console.log(step);
+// console.log(amount);
+// ----ОТПРАВКА ФОРМЫ
+refs.formEl.addEventListener('submit', startPromise);
+
+//   ----ФУНКЦИЯ КОЛБЕК ДЛЯ ОТПРАВКИ ФОРМЫ С ЗАПУСКОМ ЦИКЛА ВЫЗОВОВ ПРОМИСОВ
+function startPromise(e) {
+  e.preventDefault();
+  // --элементы формы выделяем как переменные, чтобы брать их значения
+  const {
+    elements: { delay, step, amount },
+  } = e.currentTarget;
+  console.log(delay.value);
+  console.log(step.value);
+  console.log(amount.value);
+  // -Цикл генераций промисов c их обработкой
+  for (let i = 0; i < amount; i += 1) {
+    const position = i + 1;
+    if (i > 1) {
+      delay = step.value;
+    }
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+  }
+}
+
+// --ФУНКЦИЯ ЗАПУСКА ОДНОГО ПРОМИСА
+const createPromise = (position, delay) => {
+  //--генератор случайного числа:
+  const shouldResolve = Math.random() > 0.3;
+  // --промис-------------------------
   return new Promise((resolve, reject) => {
-    const shouldResolve = Math.random() > 0.3;
-    // --Функция отложенного вызова - задержка вызова
+    // --функция отложенного вызова - задержка вызова
     setTimeout(() => {
       if (shouldResolve) {
-        resolve(data);
+        resolve({ position, delay });
       } else {
-        reject(data);
+        reject({ position, delay });
       }
     }, delay);
   });
-}
-// --Обработка промисов
-createPromise(data)
-  .then(({ position, delay }) => {
-    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  })
-  .catch(({ position, delay }) => {
-    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-  });
-//----Функция колбек для отправки формы
-// function createPromise(e) {
-//   e.preventDefault();
-//   // --Элементы формы выделяем как переменные, чтобы брать их значения
-//   const {
-//     elements: { delay, step, amount },
-//   } = e.target;
-//   // ---передаем в setInterval:
-//   const interval = Number(delay.value) + Number(step.value);
-
-//   // ---передаем в счетчик:
-//   const counterCall = Number(amount.value);
-//   // ---Генератор случайного числа:
-//   const shouldResolve = Math.random() > 0.3;
-//   // -Интервал с частотой вызова промиса
-//   setInterval(() => {
-//     // -Цикл генераций промисов
-//     for (let i = 0; i < counterCall; i += 1) {
-//       if (shouldResolve) {
-//         // --передаем номер промиса в промис:
-//         const position = i + 1;
-//         new Promise(() => {
-//           console.log(`Fulfilled promise № ${position} in ${interval} ms`);
-//         });
-//       } else {
-//         const position = i + 1;
-//         console.log(`Rejected promise № ${position} in ${interval} ms`);
-//       }
-//     }
-//   }, interval);
-// }
+};
